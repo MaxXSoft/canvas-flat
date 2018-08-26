041a8903ae3d9f47b0a38861fbddd4084126b330
MaxXSoft <x@MaxXSoft.net>
2018-08-26 10:25:06 +0800

    (test) modified the test case 'shape.cpp'

--- test/shape.cpp
+++ test/shape.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <ctime>
-#include <cstdlib>
 
 #include "../src/canvas.h"
 #include "../src/container/pngcont.h"
@@ -17,33 +15,25 @@ using namespace cvf::container;
 using namespace cvf::util;
 using namespace cvf::shape;
 
-Color GetRandColor(bool greyscale = false, bool opaque = false) {
-    auto alpha = opaque ? 1.F : (std::rand() % 100 + 1) / 100.F;
-    if (greyscale) {
-        Color::Color8b grey = std::rand() & 0xFF;
-        return Color((grey << 16) | (grey << 8) | grey, alpha);
-    }
-    else {
-        return Color(std::rand() & 0xFFFFFF, alpha);
-    }
-}
-
 int main(int argc, const char *argv[]) {
-    // initialize random generator
-    std::srand(std::time(nullptr));
     // create canvas
     Canvas<PngContainer> canvas(512, 512);
-    canvas.set_backcolor(GetRandColor(true));
+    canvas.set_backcolor(0x666666);
     canvas.set_anti_aliasing(true);
-    // add shapes to canvas
-    auto s1 = std::make_shared<Rectangle>(70, 100, 100);
-    auto s2 = std::make_shared<Rectangle>(320, 100, 100);
-    auto s3 = std::make_shared<Operation>(Operation::Opcode::Rotate, s1, PI_4);
-    canvas.AddShape(std::make_shared<Operation>(Operation::Opcode::Round, s2, 20));
-    canvas.AddShape(std::make_shared<Operation>(Operation::Opcode::Union, s1, s3));
-    // set color
-    for (const auto &shape : canvas.shapes()) {
-        shape->set_color(GetRandColor(true));
+    // create a squircle
+    auto cx = canvas.width() / 2, cy = canvas.height() / 2;
+    auto r = util::Min(cx, cy) * 3.F / 2.5F;
+    auto s1 = std::make_shared<Squircle>(cx, cy, r);
+    // generate canvas
+    auto last_rgb = canvas.backcolor().GetRGB();
+    ShapePtr temp = s1;
+    while ((r *= 0.9) >= 10) {
+        temp = std::make_shared<Operation>
+                (Operation::Opcode::Scale, temp, 0.9);
+        temp = std::make_shared<Operation>
+                (Operation::Opcode::Rotate, temp, util::PI / 12);
+        temp->set_color(last_rgb -= 0x123123);
+        canvas.AddShape(temp);
     }
     // draw & export
     canvas.Redraw();
