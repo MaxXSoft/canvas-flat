99aae20780434a0e13e5e8a33049fbd6d9fae4c2
MaxXSoft <x@MaxXSoft.net>
2018-08-29 18:07:21 +0800

    (spiral) modified the testcase

--- test/spiral.cpp
+++ test/spiral.cpp
@@ -1,3 +1,4 @@
+#include "../src/render/basic.h"
 #include "../src/canvas.h"
 #include "../src/container/pngcont.h"
 #include "../src/util/mathutil.h"
@@ -6,21 +7,27 @@
 #include "../src/shape/operation.h"
 
 using namespace cvf;
+using namespace cvf::render;
 using namespace cvf::container;
+using namespace cvf::color;
 using namespace cvf::util;
 using namespace cvf::shape;
 
 int main(int argc, const char *argv[]) {
+    // create render
+    auto render = std::make_unique<BasicRender>();
+    render->set_anti_aliasing(true);
     // create canvas
-    Canvas<PngContainer> canvas(512, 512);
+    Canvas canvas(512, 512);
     canvas.set_backcolor(0x666666);
-    canvas.set_anti_aliasing(true);
+    canvas.set_render(std::move(render));
+    canvas.set_image_container(std::make_unique<PngContainer>());
     // create a squircle
     auto cx = canvas.width() / 2, cy = canvas.height() / 2;
     auto r = Min(cx, cy) * 3.F / 2.5F;
     auto s1 = std::make_shared<Squircle>(cx, cy, r);
     // generate canvas
-    auto last_rgb = canvas.backcolor().GetRGB();
+    auto last_rgb = canvas.backcolor().GetColor().GetRGB();
     ShapePtr temp = s1;
     while ((r *= 0.9) >= 10) {
         temp = std::make_shared<Operation>
