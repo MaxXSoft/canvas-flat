e365cfcd24410360b9b28f0c2a59a0e9eef18d37
MaxXSoft <x@MaxXSoft.net>
2018-08-30 12:24:32 +0800

    (asciicont) added a container for exporting ASCII files

---
+++ src/container/asciicont.h
@@ -0,0 +1,48 @@
+#ifndef CANVASFLAT_CONTAINER_ASCIICONT_H_
+#define CANVASFLAT_CONTAINER_ASCIICONT_H_
+
+#include "imgcontainer.h"
+#include "../util/mathutil.h"
+
+namespace cvf::container {
+
+class AsciiContainer : public ImageContainer {
+public:
+    AsciiContainer() : ImageContainer() {}
+
+protected:
+    void ExportStream(std::ofstream &ofs) override {
+        for (int y = 0; y + 2 < height_; y += 3) {
+            for (int x = 0; x < width_; ++x) {
+                ofs.put(GetASCII(x, y));
+            }
+            ofs.put('\n');
+        }
+    }
+
+private:
+    const int grayscale_count = 70;
+    const char *ascii_grayscale = "$@B%8&WM#*oahkbdpqwmZO0QLC"
+            "JUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
+
+    char GetASCII(int x, int y) {
+        const auto cr = 0.2126F, cg = 0.7152F, cb = 0.0722F;
+        int pos[3] = {
+            (y * width_ + x) * 3,
+            ((y + 1) * width_ + x) * 3,
+            ((y + 2) * width_ + x) * 3,
+        };
+        int gray = 0;
+        for (const auto &i : pos) {
+            gray += buffer_[i] * cr;
+            gray += buffer_[i + 1] * cg;
+            gray += buffer_[i + 2] * cb;
+        }
+        int i = util::LinearMapping(gray / 3, 0, 255, 0, grayscale_count);
+        return ascii_grayscale[i];
+    }
+};
+
+} // namespace cvf::container
+
+#endif // CANVASFLAT_CONTAINER_ASCIICONT_H_
